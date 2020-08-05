abaf17c711ef4e718bf45112c00e9848890efd48
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-05 19:10:20 -0400

    new file for routes

---
+++ routes.py
@@ -0,0 +1,82 @@
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.models import User, Post
+from flaskblog.forms import RegistrationForm, LoginForm
+
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'First Post',
+        'content': 'This is a post.',
+        'date_posted': 'August 4, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Last Post',
+        'content': 'Post not again.',
+        'date_posted': 'December 22, 2020'
+    }
+]from models import User, Post
+
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'First Post',
+        'content': 'This is a post.',
+        'date_posted': 'August 4, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Last Post',
+        'content': 'Post not again.',
+        'date_posted': 'December 22, 2020'
+    }
+]from models import User, Post
+
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'First Post',
+        'content': 'This is a post.',
+        'date_posted': 'August 4, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Last Post',
+        'content': 'Post not again.',
+        'date_posted': 'December 22, 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=["GET", 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('You have been logged in!', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Register', form=form)
