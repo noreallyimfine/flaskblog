3d44c443206c6b57247eac40526e665b72f4a8c2
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-06 09:48:45 -0400

    add bcrypt to encrypt passwords

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '822c98eca6467896aa98a99444439fcf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
