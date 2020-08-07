f5b4070bb052e7a8d8a4e30360ba246d9e2ec0f1
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-07 10:00:40 -0400

    resize images to thumbnail size

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,6 @@
 import secrets
 import os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
@@ -78,7 +79,11 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
@@ -90,9 +95,9 @@ def account():
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
-        current_user.image_file = picture_file
         db.session.commit()
         flash('your account has been updated!',
               'success')
