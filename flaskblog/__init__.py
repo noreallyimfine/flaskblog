b7c78b7bbebf80e0461809cb61e02dd1f16f0d8e
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-07 09:51:49 -0400

    comment up to use as reference for lter

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
+# import for using sql db through flask
 from flask_sqlalchemy import SQLAlchemy
+# import for encrypting passwords
 from flask_bcrypt import Bcrypt
+# import for handling logged in sessions
 from flask_login import LoginManager
 
+# instantiate app
 app = Flask(__name__)
+# need SECRET_KEY to prevent CSRF, on deployment should be in an environment variable I believe 
 app.config['SECRET_KEY'] = '822c98eca6467896aa98a99444439fcf'
+# set database url, sqlite for dev will switch to postgres for launch
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# create objects of each connected to app
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+# set some login attributes
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
+# import down here bec routes imports from this file.
+# (The linter doesnt like this, wonder if there is another way to handle)
 from flaskblog import routes
