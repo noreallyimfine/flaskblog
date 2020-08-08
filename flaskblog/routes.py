211263f656f99384c3b49a51177bb0e30c6d91e5
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-08 19:10:52 -0400

    add delete functionality

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -182,7 +182,7 @@ def new_post():
     form = PostForm()
     # did they submit a valid form?
     if form.validate_on_submit():
-        # create a Post 
+        # create a Post
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         # add to db
         db.session.add(post)
@@ -220,3 +220,15 @@ def update_post(post_id):
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post',
                            form=form, legend="Update Post")
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
