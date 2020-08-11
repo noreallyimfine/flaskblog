676c968490fea2c4d7158cae51618da22e232d47
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-11 09:51:37 -0400

    move config to class, export secrets to environment variables

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 # import for using sql db through flask
 from flask_sqlalchemy import SQLAlchemy
@@ -7,25 +6,18 @@ from flask_bcrypt import Bcrypt
 # import for handling logged in sessions
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 # instantiate app
 app = Flask(__name__)
-# need SECRET_KEY to prevent CSRF, on deployment should be in an environment variable I believe
-app.config['SECRET_KEY'] = '822c98eca6467896aa98a99444439fcf'
-# set database url, sqlite for dev will switch to postgres for launch
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 # create objects of each connected to app
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 # set some login attributes
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('APP_TEST_EMAIL')
-app.config['MAIL_PASSWORD'] = os.environ.get('APP_TEST_EMAIL_PASS')
 mail = Mail(app)
 # import down here bec routes imports from this file.
 # (The linter doesnt like this, wonder if there is another way to handle)
